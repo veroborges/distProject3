8c0dbb658a9e2ad26e3d304746e124c3dd7c362c
Vero Borges <veronicaborges_@cmu-793273.wv.cc.cmu.edu>
2011-12-04 14:36:28 -0500

    added webapp project to repo

--- GeoServer/src/edu/cmu/eventtracker/geoserver/GeoService.java
+++ GeoServer/src/edu/cmu/eventtracker/geoserver/GeoService.java
@@ -3,6 +3,7 @@ package edu.cmu.eventtracker.geoserver;
 import edu.cmu.eventtracker.action.Action;
 
 public interface GeoService {
-	public static final long TIMEOUT = 5000;
+	public static final long TIMEOUT = 10000;
+
 	public <A extends Action<R>, R> R execute(A action);
 }
