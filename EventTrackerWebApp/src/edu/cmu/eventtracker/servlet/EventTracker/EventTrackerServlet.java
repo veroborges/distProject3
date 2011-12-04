c324c9abdd354d8e69365149565addf0722f5a15
Vero Borges <veronicaborges_@cmu-793273.wv.cc.cmu.edu>
2011-12-04 17:28:01 -0500

    actual webapp

--- EventTrackerWebApp/src/edu/cmu/eventtracker/servlet/EventTracker/EventTrackerServlet.java
+++ EventTrackerWebApp/src/edu/cmu/eventtracker/servlet/EventTracker/EventTrackerServlet.java
@@ -2,35 +2,29 @@ package edu.cmu.eventtracker.servlet.EventTracker;
 
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 
 import com.caucho.hessian.client.HessianProxyFactory;
-
-import edu.cmu.eventtracker.action.AddUserAction;
-import edu.cmu.eventtracker.action.GetUserAction;
-import edu.cmu.eventtracker.action.PingAction;
-import edu.cmu.eventtracker.dto.Event;
-//import edu.cmu.eventtracker.dto.PingResponse;
-import edu.cmu.eventtracker.geoserver.GeoService;
-import edu.cmu.eventtracker.geoserver.GeoServiceFacade;
-import edu.cmu.eventtracker.serverlocator.ServerLocator;
-import edu.cmu.eventtracker.serverlocator.ServerLocatorService;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+
+import edu.cmu.eventtracker.action.AddUserAction;
+import edu.cmu.eventtracker.action.CreateEventAction;
+import edu.cmu.eventtracker.action.LocationHeartbeatAction;
+import edu.cmu.eventtracker.dto.Event;
+import edu.cmu.eventtracker.dto.Location;
+import edu.cmu.eventtracker.dto.LocationHeartbeatResponse;
+import edu.cmu.eventtracker.geoserver.GeoService;
+import edu.cmu.eventtracker.geoserver.GeoServiceFacade;
+import edu.cmu.eventtracker.serverlocator.ServerLocatorService;
 /**
  * Servlet implementation class EventTrackerServlet
  */
@@ -39,6 +33,7 @@ public class EventTrackerServlet extends HttpServlet {
 	private HessianProxyFactory factory;
 	private InetAddress addr;
 	private Gson gson;
+	private String json;
 	ServerLocatorService locatorService;
 
 
@@ -51,30 +46,47 @@ public class EventTrackerServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    @Override
+    public void init() throws ServletException {
+    	super.init();
+		try {
+			factory = new HessianProxyFactory();
+			addr = InetAddress.getLocalHost();
+			locatorService = getServiceLocator();
+			
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {				
-//		String username = request.getParameter("username");
-//		String lat = request.getParameter("lat");
-//		String lng = request.getParameter("lon");
-//		
-//		//check if null?
-//		Double latD = Double.valueOf(lat);
-//		Double lngD = Double.valueOf(lng);
-//
-//		
-//		PingResponse res = new GeoServiceFacade(
-//				locatorService.getLocationShard(latD, lngD))
-//				.execute(new PingAction(latD, lngD, username));		
-//		
-//		List<Event> events = res.getEvents();
-//		if (res.canCreateEvent()){
-//			events.add(new Event(-1, null, null));
-//		}
-//		
-//		gson = new GsonBuilder().create();
-//		gson.toJson(res, new TypeToken<List<Event>>(){}.getType());
+		String username = request.getParameter("pUser");
+		String lat = request.getParameter("pLat");
+		String lng = request.getParameter("pLng");
+		String eventid = request.getParameter("pEventId");
+		
+		if (eventid.equals("null")){
+			eventid = null;
+		}
+		
+		double latD = Double.parseDouble(lat);
+		double lngD = Double.parseDouble(lng);
+		
+		Location loc = new Location(null, latD, lngD, username, eventid, null);
+		
+		LocationHeartbeatResponse res = new GeoServiceFacade(
+				locatorService.getLocationShard(latD, lngD))
+				.execute(new LocationHeartbeatAction(loc));		
+		
+		System.out.println("can create:" + res.canCreateEvent());
+		gson = new GsonBuilder().create();
+		String json = gson.toJson(res);
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
 	
 	}
 
@@ -83,32 +95,62 @@ public class EventTrackerServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//add user functionality
-		factory = new HessianProxyFactory();
-		addr = InetAddress.getLocalHost();
+		if (request.getParameter("mUser") != null){
+			doCreateUser(request);
+		}else if (request.getParameter("cUser") != null){
+			doCreateEvent(request);
+		}
 
-		locatorService = getServiceLocator();
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
+	
+	}
+	
+	private void doCreateEvent(HttpServletRequest request)  throws ServletException, IOException {
+		String username = request.getParameter("cUser");
+		String lat = request.getParameter("cLat");
+		String lng = request.getParameter("cLng");
+		String eventname = request.getParameter("cEventName");
 		
+		double latD = Double.parseDouble(lat);
+		double lngD = Double.parseDouble(lng);
+		
+		GeoService geoService = new GeoServiceFacade(
+				locatorService.getUserShard(username));
+
+		geoService.execute(new CreateEventAction(latD, lngD, username, eventname));
+		
+		gson = new GsonBuilder().create();
+		json = gson.toJson("created" + eventname);
+	}
+
+	private void doCreateUser(HttpServletRequest request)  throws ServletException, IOException {
 		String username = request.getParameter("mUser");
 		String name = request.getParameter("mName");
 		String password = request.getParameter("mPass");
-
+		
 		GeoService geoService = new GeoServiceFacade(
 				locatorService.getUserShard(username));
 
 		geoService.execute(new AddUserAction(username, name, password));
 		
 		gson = new GsonBuilder().create();
-		gson.toJson(username);
-	
+		json = gson.toJson(username);
 	}
-	
+
 	private ServerLocatorService getServiceLocator()
 			throws MalformedURLException {
 		
-		String locatorURL = ServerLocator.getURL(addr.getHostName(),
-				ServerLocator.SERVER_LOCATOR_PORT);
+		String locatorURL = getServiceLocatorURL(addr.getHostName(),
+				ServerLocatorService.SERVER_LOCATOR_PORT);
 		return (ServerLocatorService) factory.create(
 				ServerLocatorService.class, locatorURL);
+	}
+	
+	public static String getServiceLocatorURL(String hostname, int port) {
+		return "http://" + hostname + ":" + port + "/"
+				+ ServerLocatorService.class.getSimpleName();
 	}
 
 }
